e4723d7e4492406cd34c4efc315b8be4732b6e21
jefferson-developer-it <nhac.developer@gmail.com>
2023-01-12 10:29:21 -0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [valor_por_hora, setValorPorHora] = useState(0)
-  const [valor_por_min, setValorPorMin] = useState(0)
-  const [valor_total, setValorTotal] = useState(0)
+  const [valor_por_minuto, setValorPorMinuto] = useState(0)
+  const [valor_final, setValorFinal] = useState(0)
   const [rodando, setRodando] = useState(false)
-  const [pausado, setPause] = useState(false)
-  const [histórico, setHistórico] = useState([])
-  const [minutos_rodado, setMinutosRodado] = useState(0)
-  const [time, setTime] = useState(undefined)
+  const [pausada, setPausada] = useState(false)
+  const [minutos_rodados, setMinutosRodados] = useState(0)
+  const [histórico, setHistórico] = useState(JSON.parse(localStorage["history"]) || [])
+  const [time, setTime] = useState(undefined);
+  const fomatador_real = Intl.NumberFormat("pt-br", {
+    currency: "BRL",
+    style: "currency"
+  })
 
   useEffect(()=>{
-    setValorPorMin(valor_por_hora / 60);
-  }, [valor_por_hora])
 
-  useEffect(()=>{
-    if(rodando && !pausado){
+    if(rodando && !pausada){
       setTime(setTimeout(()=>{
-        let time_run = minutos_rodado + 1;
-        setMinutosRodado(time_run);
+        let minutos = minutos_rodados + 1;
+        setMinutosRodados(minutos)
 
-        setValorTotal(parseFloat(valor_por_min) * time_run)
-
+        setValorFinal(Number(parseFloat(valor_por_minuto * minutos).toFixed(2)))
+        
         clearTimeout(time)
       }, 1000 * 60))
-
-    }else if(time){
-      clearTimeout(time)
     }
 
-  }, [rodando, pausado, minutos_rodado])
-
+  }, [rodando, pausada, minutos_rodados])
+  
+  useEffect(()=>{
+    localStorage["history"] = JSON.stringify(histórico)
+  }, [histórico])
 
   return (
     <div className="card">
@@ -39,49 +39,59 @@ function App() {
         <div className='in-line'>
           <div className='input'>
             <input
-              readOnly={rodando}
-              onChange={(e)=>{
-                setValorPorHora(e.target.value);
+              onChange={({target})=>{
+                if(target.value){
+                  setValorPorMinuto(Number(parseFloat(target.value / 60).toFixed(2)))
+                }
               }}
               type={"number"} min={"0"} placeholder="Valor que você recebe"/>
             <p>/hora</p>
           </div>
           <div className='control'>
-            <p>Valor p/ minuto: {parseFloat(valor_por_min).toFixed(2)}R$/min | Tempo passado: {minutos_rodado} min</p>
-            <h2 className='valor-receber'>{parseFloat(valor_total).toFixed(2)}R$</h2>
-              {
-                rodando? (
-                  <>
-                    <button onClick={()=> setPause(!pausado)} className='pause'>{pausado? "Retomar": "Pausar"}</button>
-                    <button onClick={()=>{
-                      setRodando(false)
-                      setHistórico([...histórico, {
-                        minutos_rodado,
-                        valor_por_min,
-                        valor_por_hora,
-                        valor_total,
-                        data: Date.now()
-                      }])
+            <p>Valor p/ minuto: {valor_por_minuto}R$/min | Tempo passado: {minutos_rodados} min</p>
+            <h2 className='valor-receber'>{valor_final}R$</h2>
+            
+            {
+              rodando? <>
+                <button onClick={()=> setPausada(!pausada)} className='pause'>{pausada? "Retomar": "Pausar"}</button>
+                <button onClick={()=>{
+                    setRodando(false)
+                    setPausada(false)
 
-                      setMinutosRodado(0)
-                      setValorTotal(0)
-                      setPause(false)
-                    }}  className='stop'>Parar</button>
-                  </>
-                ): <button onClick={()=> setRodando(true)} className='start'>Iniciar</button>
-              }
+                    const para_histórico = {
+                      minutos_rodados, 
+                      valor_por_minuto, 
+                      valor_final,
+                      data: Date.now(),
+                      valor_por_hora: valor_por_minuto * 60
+                    }
+
+                    setHistórico([para_histórico, ...histórico])
+                    setMinutosRodados(0)
+                    setValorFinal(0)
+                }} className='stop'>Parar</button>
+              </>: <button onClick={()=>{
+                if(valor_por_minuto) setRodando(true)
+                else alert("Adicione o valor a ser contabilizado.")
+              }} className='start'>Iniciar</button>
+            }
+            
+            
+
 
           </div>
           <div>
             <h3>Histórico</h3>
             <p>Minutos trabalhados | Valor por hora | Valor por minuto | Total | Data</p>
             {
-              histórico.map(({minutos_rodado, valor_por_min, data, valor_por_hora, valor_total})=>{
+              histórico.map(({minutos_rodados, valor_por_hora, valor_por_minuto, valor_final, data})=>{
                 return (
-                    <div key={`${(Math.random()*199).toString(16)}`}>
-                      <p>{minutos_rodado} minuto/s | {parseFloat(valor_por_hora).toFixed(2)}R$/h | {parseFloat(valor_por_min).toFixed(2)}R$/min | {parseFloat(valor_total).toFixed(2)}R$| {new Date(data).toLocaleString("pt-br")}</p>
-                    </div>
-                  )
+                  <div key={(Math.random() * 100).toString(16)}>
+                    <p>
+                      {minutos_rodados} | {fomatador_real.format(valor_por_hora)} | {fomatador_real.format(valor_por_minuto)} | {fomatador_real.format(valor_final)} | {new Date(data).toLocaleString("pt-br")}
+                    </p>
+                  </div>
+                )
               })
             }
           </div>
